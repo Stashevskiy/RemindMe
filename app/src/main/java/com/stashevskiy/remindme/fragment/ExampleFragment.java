e585429fb84b14997518085c00aaf04e8c6c5910
Serega Stashevkiy <stashevskiy.1994@gmail.com>
2018-03-06 16:57:10 +0200

    Implemented navigation view and tab navigation with test fragment

---
+++ app/src/main/java/com/stashevskiy/remindme/fragment/ExampleFragment.java
@@ -0,0 +1,31 @@
+package com.stashevskiy.remindme.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.stashevskiy.remindme.R;
+
+public class ExampleFragment extends Fragment{
+
+    private static final int LAYOUT = R.layout.fragment_example;
+
+    View view;
+
+    public static ExampleFragment getInstance(){
+        Bundle args = new Bundle();
+        ExampleFragment fragment = new ExampleFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(LAYOUT, container, false);
+        return view;
+    }
+}
